c23435c9392d74b10e908d2718eec009aabe5154
Yusuke Kido <y.k@is.s.u-tokyo.ac.jp>
2017-01-01 20:48:06 +0900

    Set up sinatra-asset-pipeline

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
+source 'https://rails-assets.org'
 
+gem 'rails-assets-jquery'
 gem 'sinatra'
+gem 'sinatra-asset-pipeline'
 
 group :development do
   gem 'sinatra-reloader', require: 'sinatra/reloader'
--- app.rb
+++ app.rb
@@ -1,15 +1,32 @@
+require 'bundler/setup'
+
+ENV['RACK_ENV'] ||= 'development'
+Bundler.require(:default, ENV['RACK_ENV'])
+
 module Lukas
   require 'sinatra/base'
 
   class App < Sinatra::Base
+    require 'sinatra/asset_pipeline'
     require 'sinatra/reloader' if development?
+
+    Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+    configure do
+      set :assets_css_compressor, :sass
+      register Sinatra::AssetPipeline
+
+      RailsAssets.load_paths.each do |path|
+        settings.sprockets.append_path(path)
+      end
+    end
 
     configure :development do
       register Sinatra::Reloader
     end
 
     get '/' do
-      'ruby sucks.'
+      erb :index
     end
   end
 end
